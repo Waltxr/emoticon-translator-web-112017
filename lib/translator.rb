d04a7f40ce9b545dd078b0b3fe6acb89333241d3
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-13 22:15:32 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,6 +21,7 @@ def get_japanese_emoticon(file_path, emoticon)
     return load_library(file_path)["get_meaning"]
   else
     return "Sorry, that emoticon was not found"
+  end
 end
 
 # def get_english_meaning(file_path, emoticon)
