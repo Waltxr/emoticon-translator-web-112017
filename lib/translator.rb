4a7abffd2e46160c80b9a35d3317e08f51dabbb6
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-13 20:51:13 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,8 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  # code goes here
+  puts load_library(file_path)["get_emoticon"]
+  
 end
 
 def get_english_meaning
