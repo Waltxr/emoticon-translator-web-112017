f4a148100db2eef724ddfb46342426d791c7ce4c
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-13 21:21:59 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  if load_library(file_path)["get_emoticon"][emoticon]
+  if load_library(file_path)["get_emoticon"][emoticon] == nil
     puts "Sorry, that emoticon was not found"
   else
     return load_library(file_path)["get_emoticon"][emoticon]
