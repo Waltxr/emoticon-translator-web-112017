2d9cb8d81f17fddb9dc66cb09e9a72dd36e4512f
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-13 20:09:22 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 
-def load_library
-  emoticons = YAML.load_file('./lib/emoticons.yml')
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
 
   return emoticons
 end
