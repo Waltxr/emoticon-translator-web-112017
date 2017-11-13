04e896c96200ebd8a5bc159084e49cfb9d6644d4
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-13 20:28:34 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,16 +5,18 @@ def load_library(file_path)
 
   emoticons = {
     :get_meaning => {
-       
+
     }
 
     :get_emoticon => {
-      
+
     }
   }
 
   emoticons_data.each do |meaning, emoticon|
-  
+    emoticons[:get_meaning]=emoticon[1]
+    emoticonsemoticons[:get_meaning][emoticon[1]]=meaning.to_s
+
 end
 
 def get_japanese_emoticon
