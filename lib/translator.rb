f1ca7a8db295eebf878a5ad82471b5bc6515a3f4
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-13 22:18:27 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,10 +17,7 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  if load_library(file_path)["get_emoticon"][emoticon]
-    return "Sorry, that emoticon was not found"
-  else
-    return load_library(file_path)["get_emoticon"][emoticon]
+  return load_library(file_path)["get_emoticon"][emoticon]
 end
 # def get_english_meaning
 #   # code goes here
